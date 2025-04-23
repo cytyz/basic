ceb784df03851ac1e5bcd1814165d7f16b3a0f70
cytyz <337196387@qq.com>
2025-04-23 18:36:03 +0800

    角谷猜想

--- branch_loop/jiaogu.py
+++ branch_loop/jiaogu.py
@@ -7,8 +7,8 @@ n = int(input("请输入一个正整数："))
 while n > 0:
     # 判断 n 是否可以被 2 整除 #
     if n % 2 == 0:
-        print(n, " / 2 = ", int(n / 2))
-        n = int(n / 2)
+        print(n, " / 2 = ", n // 2)
+        n = n // 2
     else:
         print(n, " * 3 + 1 == ", n * 3 + 1)
         n = n * 3 + 1
