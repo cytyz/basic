94c594e24b4d3e897b682683d9e16c4d1a569270
cytyz <337196387@qq.com>
2025-04-29 09:48:52 +0800

    回文数

--- branch_loop/Palindrome.py
+++ branch_loop/Palindrome.py
@@ -1,6 +1,9 @@
 # 判断一个整数是否为回文数
 # 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
 num = int(input("请输入一个正整数："))
+# 使用切片
+# print("是回文数") if num == num[::-1] else print("不是回文数")
+
 num_temp = num
 if num < 0 or num % 10 == 0:
     print("不是回文数")
